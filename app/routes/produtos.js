50c69e7d2aa05e60a4c9cb502a6e68d31006e8e0
Vinicius Morais <vinicius.reis.morais@gmail.com>
2018-03-29 18:19:51 -0300

    refactoring validacoes

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,6 @@
 const ProdutoService = require('../services/ProdutoService');
 
-const { check, validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator/check');
 
 module.exports = (app) => {
   app.get('/produtos', (req, res) => {
@@ -21,32 +21,15 @@ module.exports = (app) => {
       });
   });
 
-  app.post('/produto', [
-    check('titulo', 'Titulo deve possuir no minimo 3 caracteres')
-      .isLength({ min: 3 }),
-    check('descricao', 'Descricao deve possuir no minimo 10 caracteres')
-      .isLength({ min: 10 }),
-    check('status', 'Status invalido')
-      .custom(value => new Promise((resolve, reject) => {
-        if (value !== 'ativo' && value !== 'edicao') {
-          reject();
-        }
-        resolve();
-      })),
-    check('link', 'Link nao eh uma URL valida')
-      .isURL(),
-    check('imagens', 'E necessario informar ao menos uma imagem')
-      .custom(value => new Promise((resolve, reject) => {
-        if (value.length < 1 || typeof value !== 'object') {
-          reject();
-        }
-        resolve();
-      })),
-  ], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.mapped() });
-    }
-    return res.send('OK');
-  });
+  app.post(
+    '/produto',
+    ProdutoService.validaProduto(false),
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.mapped() });
+      }
+      return res.send('OK');
+    },
+  );
 };
--- app/services/ProdutoService.js
+++ app/services/ProdutoService.js
@@ -1,5 +1,6 @@
 const ProdutoDAO = require('../db/ProdutoDAO');
 const clientFactory = require('../db/connectionFactory');
+const { check } = require('express-validator/check');
 
 module.exports = class ProdutoService {
   static listarProdutos() {
@@ -16,5 +17,41 @@ module.exports = class ProdutoService {
         const produtoDAO = new ProdutoDAO(client);
         return produtoDAO.listarProdutosAtivos();
       });
+  }
+
+  static incluiProduto() {
+    return clientFactory()
+      .then((client) => {
+        const produtoDAO = new ProdutoDAO(client);
+        return produtoDAO.incluiProduto();
+      });
+  }
+  static validaProduto(isAlteracao) {
+    const validacoes = [
+      check('titulo', 'Titulo deve possuir no minimo 3 caracteres')
+        .isLength({ min: 3 }),
+      check('descricao', 'Descricao deve possuir no minimo 10 caracteres')
+        .isLength({ min: 10 }),
+      check('status', 'Status invalido')
+        .custom(value => new Promise((resolve, reject) => {
+          if (value !== 'ativo' && value !== 'edicao') {
+            reject();
+          }
+          resolve();
+        })),
+      check('link', 'Link nao eh uma URL valida')
+        .isURL(),
+      check('imagens', 'E necessario informar ao menos uma imagem')
+        .custom(value => new Promise((resolve, reject) => {
+          if (value.length < 1 || typeof value !== 'object') {
+            reject();
+          }
+          resolve();
+        })),
+    ];
+    if (isAlteracao) {
+      validacoes.push(check('id', 'Nao foi possivel identificar o produto').exists());
+    }
+    return validacoes;
   }
 };
